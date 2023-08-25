57ae0c0b34fcf1c092982b7fdf51cd642b030cab
tudastuk <129007673+tudastuk@users.noreply.github.com>
2023-08-25 21:55:05 +0300

    Третье задание выполнено

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,8 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let result = users.filter( user => user.gender === gender).reduce((acc, current, index, array) => acc + current.age / array.length, 0);
+    return result;
 }
