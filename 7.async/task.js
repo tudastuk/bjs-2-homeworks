db2f328f83ba5e7872be457e6786ffdaa0be202f
tudastuk <129007673+tudastuk@users.noreply.github.com>
2023-08-25 22:27:45 +0300

    Седьмое задание выполнено

---
+++ 7.async/task.js
@@ -0,0 +1,66 @@
+class AlarmClock {
+    constructor() {
+      this.alarmCollection = [];
+      this.intervalId = null;
+    }
+  
+    addClock(time, callback) {
+      if (!time || !callback) {
+        throw new Error("Отсутствуют обязательные аргументы");
+      }
+  
+      if (this.alarmCollection.some((alarm) => alarm.time === time)) {
+        console.warn("Уже присутствует звонок на это же время");
+      }
+  
+      this.alarmCollection.push({
+        time,
+        callback,
+        canCall: true,
+      });
+    }
+  
+    removeClock(time) {
+      this.alarmCollection = this.alarmCollection.filter(
+        (alarm) => alarm.time !== time
+      );
+    }
+  
+    getCurrentFormattedTime() {
+      const now = new Date();
+      return now.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+    }
+  
+    start() {
+      if (this.intervalId) {
+        return;
+      }
+  
+      this.intervalId = setInterval(() => {
+        const currentTime = this.getCurrentFormattedTime();
+  
+        this.alarmCollection.forEach((alarm) => {
+          if (alarm.time === currentTime && alarm.canCall) {
+            alarm.canCall = false;
+            alarm.callback();
+          }
+        });
+      }, 1000);
+    }
+  
+    stop() {
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    }
+  
+    resetAllCalls() {
+      this.alarmCollection.forEach((alarm) => {
+        alarm.canCall = true;
+      });
+    }
+  
+    clearAlarms() {
+      this.stop();
+      this.alarmCollection = [];
+    }
+  }
