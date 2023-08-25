ed8da7e8c9b0cef1df8cbaf85abd60360491ae70
tudastuk <129007673+tudastuk@users.noreply.github.com>
2023-08-25 22:50:49 +0300

    Восьмое задание выполнено

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,42 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
+	let cache = [];
   
-}
-
-//Задача № 2
-function debounceDecoratorNew(func, delay) {
+	function wrapper(...args) {    
+	  const hash = md5(JSON.stringify(args));    
+	  let objectInCache = cache.find((obj) => obj.hash === hash);
+	  if (objectInCache) {      
+		console.log(`Из кэша: ${objectInCache.result}`);
+		return `Из кэша: ${objectInCache.result}`;
+	  }
+	  let result = func(...args);    
+	  cache.push({ hash, result });     
+	  if (cache.length > 5) {
+		cache.shift();     
+	  }
+	  console.log(`Вычисляем: ${result}`);
+	  return `Вычисляем: ${result}`;  
+	}  
+	return wrapper;
+  }
   
-}
+	
+  //Задача № 2
+  function debounceDecoratorNew(func, delay) {
+	let timer = null;
+	function wrapper(...args) {
+	  if (timer === null) {
+		func(...args);
+		wrapper.count++;
+	  }
+	  clearTimeout(timer);
+	  timer = setTimeout(function() {
+		func(...args);
+		wrapper.count++;
+	  }, delay);
+	  wrapper.allCount++;
+	}
+	wrapper.count = 0;
+	wrapper.allCount = 0;
+	return wrapper;
+  }
